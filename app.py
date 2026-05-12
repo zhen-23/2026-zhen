a5a2f602025b25e149cde3333b7556770c418664
zhen-23 <a0923930343@gmail.com>
2026-05-12 11:28:51 +0800

    webhook 3

--- app.py
+++ app.py
@@ -36,8 +36,12 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req["queryResult"]["action"]
-    msg =  req["queryResult"]["queryText"]
-    info = "我是蔡純珍設計的電影聊天機器人, 動作：" + action + "； 查詢內容：" + msg
+    #msg =  req["queryResult"]["queryText"]
+    #info = "我是楊子青設計的電影聊天機器人, 動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req["queryResult"]["parameters"]["rate"]
+        info = "我是蔡純珍設計的電影聊天機器人, 您選擇的電影分級是：" + rate
+
     return make_response(jsonify({"fulfillmentText": info}))
 
     @app.route("/rate")
