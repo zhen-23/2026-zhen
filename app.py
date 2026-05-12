73038655cca2d556f06a26290cdbd53d946fc519
zhen-23 <a0923930343@gmail.com>
2026-05-12 11:19:56 +0800

    webhook 2

--- app.py
+++ app.py
@@ -30,14 +30,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-    @app.route("/webhook", methods=["POST"])
+@app.route("/webhook", methods=["POST"])
 def webhook():
     # build a request object
     req = request.get_json(force=True)
     # fetch queryResult from json
-    action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "動作：" + action + "； 查詢內容：" + msg
+    action =  req["queryResult"]["action"]
+    msg =  req["queryResult"]["queryText"]
+    info = "我是蔡純珍設計的電影聊天機器人, 動作：" + action + "； 查詢內容：" + msg
     return make_response(jsonify({"fulfillmentText": info}))
 
     @app.route("/rate")
