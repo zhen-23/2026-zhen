fa3b04aead74a80997d39d6b0a846c1958dc175d
zhen-23 <a0923930343@gmail.com>
2026-05-27 20:38:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,25 @@
-#pip install google-genai
 from google import genai
-
 from google.genai import types
-
-#from PIL import Image
-#import numpy as np, easyocr
-
+import requests
 from bs4 import BeautifulSoup
-
 from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
 import random
-
 import os
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# 判斷是在 Vercel 還是本地
 if os.path.exists('serviceAccountKey.json'):
-    # 本地環境：讀取檔案
     cred = credentials.Certificate('serviceAccountKey.json')
 else:
-    # 雲端環境：從環境變數讀取 JSON 字串
     firebase_config = os.getenv('FIREBASE_CONFIG')
     cred_dict = json.loads(firebase_config)
     cred = credentials.Certificate(cred_dict)
 
 firebase_admin.initialize_app(cred)
 
-
-
 app = Flask(__name__)
-
-
 client = genai.Client()
 
 @app.route("/")
@@ -46,13 +32,10 @@ def messenger():
 
 @app.route("/AI")
 def AI():
-    # 每次使用者拜訪該路徑時，直接使用全域的 client 呼叫模型
     response = client.models.generate_content(
         model='gemini-3.5-flash',
         contents='我想查詢靜宜大學資管系的評價？',
     )
-    
-    # 回傳生成的文字
     return response.text
 
 @app.route('/ask', methods=['GET', 'POST']) 
@@ -69,24 +52,14 @@ def ask():
             return response.text
         except Exception as e:
             return f"發生錯誤: {str(e)}", 500
-
     else:    
-        # 當使用者直接打開網頁 (GET) 時，顯示輸入框畫面
         return render_template("ask.html")
-
-@app.route("/webdemo")
-def webdemo():
-    return render_template("webdemo.html")
-
-@app.route("/demo")
-def demo():
-    return render_template("demo.html")
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     req = request.get_json(force=True)
     action = req["queryResult"]["action"]
-    msg = req["queryResult"]["action"]
+    msg = req["queryResult"]["queryText"]
     
     if (action == "rateChoice"):
         rate = req["queryResult"]["parameters"]["rate"]
@@ -104,9 +77,9 @@ def webhook():
     else:
         try:
             ai_config = types.GenerateContentConfig(
-                max_output_tokens = 800
+                max_output_tokens = 600
             )
-            prompt_work = f"請針對問題『{msg}』，用繁體中文直接寫出一篇大約 500 字左右的詳細介紹。請直接分段、條列重點（例如課程特色、就業方向），不要說任何廢話，速度越快越好。"
+            prompt_work = f"請針對主題『{msg}』，用繁體中文寫出一篇大約 350 字到 450 字左右的詳細特色介紹。直接分點列出，不要說任何廢話客套話，字數要充足。"
             response = client.models.generate_content(
                 model='gemini-3.5-flash',
                 contents=prompt_work,
